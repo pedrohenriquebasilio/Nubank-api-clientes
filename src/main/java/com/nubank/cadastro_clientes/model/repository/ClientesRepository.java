7aaa924530da4b91f99daafeabbd42c9805e04cc
Pedro <phbasilio6903@gmail.com>
2025-04-17 09:18:38 -0300

    feat: adicionei os repositorys

--- src/main/java/com/nubank/cadastro_clientes/model/repository/ClientesRepository.java
+++ src/main/java/com/nubank/cadastro_clientes/model/repository/ClientesRepository.java
@@ -2,5 +2,5 @@ package com.nubank.cadastro_clientes.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface clientes extends JpaRepository<clientes, Long> {
+public interface ClientesRepository extends JpaRepository<ClientesRepository, Long> {
 }
--- src/main/java/com/nubank/cadastro_clientes/model/repository/ContatosRepository.java
+++ src/main/java/com/nubank/cadastro_clientes/model/repository/ContatosRepository.java
@@ -2,5 +2,5 @@ package com.nubank.cadastro_clientes.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface contatos extends JpaRepository <contatos, Long> {
+public interface ContatosRepository extends JpaRepository <ContatosRepository, Long> {
 }
